77c219709235da572ca4dc43587ce40de0e96ad7
jppboi <allenftc@outlook.com>
2022-12-27 14:46:49 -0800

    lol

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CamTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@TeleOp(name="CVTEST")
+public class CamTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        WebcamName camName =  hardwareMap.get(WebcamName.class, "camera");
+        OpenCvCamera cam = OpenCvCameraFactory.getInstance().createWebcam(camName);
+        cam.openCameraDevice();
+        PipelineKiwi pipeline = new PipelineKiwi();
+        cam.setPipeline(pipeline);
+        cam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+        pipeline.setColor(PipelineKiwi.DetectType.POLE);
+
+        while (!isStopRequested()) {
+            telemetry.addData("x",pipeline.getX());
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConeGrab.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConeGrab.java
@@ -5,15 +5,18 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DigitalChannelController;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @TeleOp(name="Cone Grab")
 public class ConeGrab extends LinearOpMode {
-Servo leftArm,rightArm,frontArm,wrist,claw;
-DcMotor turret,slide;
+Servo leftArm,rightArm,frontArm,wrist,claw,tilt;
+DcMotor turret,slide, rSlide,lSlide;
 Rev2mDistanceSensor dist;
 DigitalChannel magnet;
     @Override
@@ -26,12 +29,32 @@ DigitalChannel magnet;
         wrist = hardwareMap.get(Servo.class,"wrist");
         claw = hardwareMap.get(Servo.class,"claw");
         turret = hardwareMap.get(DcMotor.class,"turret");
+        turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slide = hardwareMap.get(DcMotor.class,"slide");
+        rSlide = hardwareMap.get(DcMotor.class,"upleft");
+        rSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lSlide = hardwareMap.get(DcMotor.class,"upright");
+        lSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        tilt = hardwareMap.get(Servo.class,"slides");
         dist = hardwareMap.get(Rev2mDistanceSensor.class,"dist");
         magnet = hardwareMap.get(DigitalChannel.class,"magnet");
         magnet.setMode(DigitalChannel.Mode.INPUT);
+        double tiltAmount = 0.5;
+
+        WebcamName camName =  hardwareMap.get(WebcamName.class, "camera");
+        OpenCvCamera cam = OpenCvCameraFactory.getInstance().createWebcam(camName);
+        cam.openCameraDevice();
+        PipelineKiwi pipeline = new PipelineKiwi();
+        cam.setPipeline(pipeline);
+        cam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+        pipeline.setColor(PipelineKiwi.DetectType.POLE);
+
         while(!isStopRequested()) {
-            if (dist.getDistance(DistanceUnit.INCH)<1.5&&!gamepad1.left_bumper&&dist.getDistance(DistanceUnit.INCH)!=0.0) {
+
+            /*if (dist.getDistance(DistanceUnit.INCH)<1.5&&!gamepad1.left_bumper&&dist.getDistance(DistanceUnit.INCH)!=0.0) {
                // claw.setPosition(0.3);
             }
             else {
@@ -47,19 +70,59 @@ DigitalChannel magnet;
                 wrist.setPosition(0);
             if (gamepad1.b)
                 wrist.setPosition(1);
-            }
+            }*/
             //turret.setPower(gamepad1.right_stick_x);
             //slide.setPower(gamepad1.right_stick_y);
            // telemetry.addData("arm pos", leftArm.getPosition());
             //telemetry.addData("distance", dist.getDistance(DistanceUnit.INCH));
             //telemetry.addData("magnet",magnet.getState());
             //telemetry.addData("cones",cones);
-            //TODO: Reverse Front Arm
+            tiltAmount+=gamepad1.left_stick_y/100;
+            sleep(10);
+            turret.setPower(gamepad1.left_stick_x/2);
+            rSlide.setPower(gamepad1.right_stick_y);
+            tilt.setPosition(tiltAmount);
+            telemetry.addData("tilt",tiltAmount);
+            telemetry.addData("turret",turret.getCurrentPosition());
+            telemetry.addData("slide", rSlide.getCurrentPosition());
+            telemetry.update();// pole is 27 in above axis of rotation
+            if (gamepad1.a) {
+                if (pipeline.getX()!=-404)
+                    turret.setPower((pipeline.getX()-180)/360);
+            }
+            if (gamepad1.x) {
+                turret.setTargetPosition(-365);
+                turret.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                turret.setPower(1);
+                rSlide.setTargetPosition(-2550);
+                rSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rSlide.setPower(1);
+                tilt.setPosition(0.27);
+                while (turret.isBusy()|| rSlide.isBusy()) {
+
+                }
+                rSlide.setTargetPosition(5);
+                rSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rSlide.setPower(1);
+                while (turret.isBusy()|| rSlide.isBusy()) {
+
+                }
+                turret.setTargetPosition(0);
+                turret.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                turret.setPower(1);
+                tilt.setPosition(1);
+                while (turret.isBusy()|| rSlide.isBusy()) {
+
+                }
+                turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                rSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+
             if (gamepad1.y) {
                 wrist.setPosition(0);
-                leftArm.setPosition(0.38+(cones-5)/20.0);
-                rightArm.setPosition(1-(0.38+(cones-5)/20.0));
-                frontArm.setPosition(0.68+(cones-5)/20.0);
+                leftArm.setPosition(0.38+(cones-6)/20.0);
+                rightArm.setPosition(1-(0.38+(cones-6)/20.0));
+                frontArm.setPosition(0.68+(cones-6)/20.0);
                 telemetry.addData("arm pos", leftArm.getPosition());
                 telemetry.update();
                 while (dist.getDistance(DistanceUnit.INCH)>1.5||dist.getDistance(DistanceUnit.INCH)==0.0) {
@@ -68,10 +131,10 @@ DigitalChannel magnet;
                         slide.setPower(0);
                     }
                     else if (dist.getDistance(DistanceUnit.INCH)<10&&dist.getDistance(DistanceUnit.INCH)!=0) {
-                        slide.setPower(0.4);
+                        slide.setPower(0.25);
                     }
                     else {
-                        slide.setPower(0.8);
+                        slide.setPower(0.5);
                     }
                 }
                 claw.setPosition(0.3);
@@ -91,7 +154,7 @@ DigitalChannel magnet;
                     leftArm.setPosition(0.8);
                     rightArm.setPosition(0.2);
                     sleep(250);
-                    claw.setPosition(0.6);
+                    claw.setPosition(0.54);
                     sleep(300);
                     leftArm.setPosition(0.5);
                     rightArm.setPosition(0.5);
